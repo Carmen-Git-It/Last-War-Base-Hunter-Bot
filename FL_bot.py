36cad97b779a8bda9f1e001a55ec06367e0ea391
CarmenGitIt <carmenwhitton@gmail.com>
2024-12-04 18:46:25 -0500

    new input method in bot 2.0, keeping old version as a reference

--- FL_bot.py
+++ FL_bot.py
@@ -1,13 +1,19 @@
 import time
 import csv
 
+import PIL.Image
+import cv2
 import pyautogui as pg
+import pytesseract as pt
+import numpy as np
+from fuzzywuzzy import fuzz, process
 
 APP_DRAWER = (1905, 1023)
-CLOSE_APP = (1842, 198)
+CLOSE_APP = (1815, 225)
 OPEN_APP = (1779, 215)
 
-KIM_POS = (1805, 170)
+KIM_POS = (1800, 162)
+AD_POS = (1819, 107)
 
 AVATAR_POS = (1366,77)
 SERVER_POS = (1733, 722)
@@ -23,44 +29,87 @@ NAME_PLATE_TOPLEFT = (1458, 231)
 NAME_PLATE_TOPRIGHT = (1768, 231)
 NAME_PLATE_BOTLEFT = (1458, 269)
 NAME_PLATE_BOT_RIGHT = (1762, 269)
-CLOSE_LIST = (1846, 135)
+CLOSE_LIST = (1840, 135)
 # TODO:
 ACCEPT = (1742, 264)
 ACCEPT_OFFSET = (1742, 237)
 DENY = (1803, 260)
 
+# LOST OR GAINED TITLES
+ALT_FL = (1430, 594)
+ALT_STRATEGY = (1603, 594)
+ALT_SECURITY = (1765, 594)
+ALT_DEVELOPMENT = (1425, 792)
+ALT_SCIENCE = (1602, 792)
+ALT_INTERIOR = (1771, 792)
+
+ALLIANCE_TOPLEFT = (1456,239)
+ALLIANCE_TOPRIGHT = (1528, 239)
+ALLIANCE_BOTLEFT = (1456, 262)
+ALLIANCE_BOTRIGHT = (1528, 262)
+
+MILITARY_COMMANDER = (1506, 216)
+ADMINISTRATIVE_COMMANDER = (1691, 217)
+
 def close_app():
     pg.mouseDown(APP_DRAWER)
     pg.mouseUp(APP_DRAWER)
 
-    time.sleep(4)
+    time.sleep(8)
 
     pg.mouseDown(CLOSE_APP)
     pg.mouseUp(CLOSE_APP)
 
-    time.sleep(4)
+    time.sleep(8)
 
 def open_app():
     pg.mouseDown(OPEN_APP)
     pg.mouseUp(OPEN_APP)
 
-    time.sleep(40)
+    time.sleep(60)
+
+    pg.mouseDown(AD_POS)
+    pg.mouseUp(AD_POS)
+
+    time.sleep(8)
 
     pg.mouseDown(KIM_POS)
     pg.mouseUp(KIM_POS)
 
-    time.sleep(4)
+    time.sleep(8)
+
+    pg.mouseDown(KIM_POS)
+    pg.mouseUp(KIM_POS)
+
+    time.sleep(8)
+
+    pg.mouseDown(KIM_POS)
+    pg.mouseUp(KIM_POS)
+
+    time.sleep(8)
+
+    pg.mouseDown(AD_POS)
+    pg.mouseUp(AD_POS)
+
+    time.sleep(8)
 
 def go_to_titles():
     # Avatar
     pg.mouseDown(AVATAR_POS)
     pg.mouseUp(AVATAR_POS)
 
-    time.sleep(1)
+    time.sleep(3)
 
     # Server
     pg.mouseDown(SERVER_POS)
     pg.mouseUp(SERVER_POS)
+
+    time.sleep(3)
+
+    pg.moveTo(1610, 710)
+    pg.dragRel(0, -300, duration=1)
+
+    time.sleep(2)
 
 def apply_fl():
     # FL
@@ -77,9 +126,8 @@ def accept_top_applicant(high_demand):
     pg.mouseUp(LIST)
     time.sleep(1)
 
-    #TODO: Intelligently detect high demand
+    if high_demand == True:
 
-    if high_demand:
         pg.moveTo(1588, 246)
         pg.dragRel(0, 500, duration=0.5)
 
@@ -103,6 +151,29 @@ def accept_top_applicant(high_demand):
 
         time.sleep(0.5)
 
+    # Get image of their alliance tag
+    image = np.asarray(pg.screenshot(region=(1456, 239, 85, 23)))
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.copyMakeBorder(image_gray, 40, 40, 60, 60, cv2.BORDER_REPLICATE)
+    image = cv2.threshold(image_gray, 55, 255, cv2.THRESH_BINARY)[1]
+    image = cv2.resize(image, (0, 0), fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
+
+    # Comment this out in prod
+    # image = PIL.Image.fromarray(image)
+    # image.show()
+
+    result = pt.image_to_string(image, config='--psm 6')
+    result = result.replace("\x0c", "").replace("\\", "").replace("/", "")
+    result = result.split("\n")
+    print(result)
+
+
+    if result[0] and result[0] == "[Nova]":
+        print("Detected Nova, reject.")
+    #else:
+
+    #TODO: in prod, reject or accept in if/else
+
     pg.mouseDown(ACCEPT)
     pg.mouseUp(ACCEPT)
     time.sleep(1)
@@ -115,54 +186,63 @@ def accept_top_applicant(high_demand):
     pg.mouseUp(CLOSE_LIST)
     time.sleep(1)
 
-def accept_loop():
+def accept_loop(extra_titles):
     # print("loop")
 
+    strat = STRATEGY
+    sec = SECURITY
+    dev = DEVELOPMENT
+    sci = SCIENCE
+    inter = INTERIOR
+
+    if extra_titles:
+        strat = ALT_STRATEGY
+        sec = ALT_SECURITY
+        dev = ALT_DEVELOPMENT
+        sci = ALT_SCIENCE
+        inter = ALT_INTERIOR
+
     # Strategy
-    pg.mouseDown(STRATEGY)
-    pg.mouseUp(STRATEGY)
+    pg.mouseDown(strat)
+    pg.mouseUp(strat)
     time.sleep(1)
 
-    accept_top_applicant(False)
+    accept_top_applicant(True)
 
     # Security
-    pg.mouseDown(SECURITY)
-    pg.mouseUp(SECURITY)
+    pg.mouseDown(sec)
+    pg.mouseUp(sec)
     time.sleep(1)
 
-    accept_top_applicant(False)
-
-    high_demand = True
+    accept_top_applicant(True)
 
     # Development
-    pg.mouseDown(DEVELOPMENT)
-    pg.mouseUp(DEVELOPMENT)
+    pg.mouseDown(dev)
+    pg.mouseUp(dev)
     time.sleep(1)
 
     accept_top_applicant(True)
-
-    high_demand = True
 
     # SCIENCE
-    pg.mouseDown(SCIENCE)
-    pg.mouseUp(SCIENCE)
+    pg.mouseDown(sci)
+    pg.mouseUp(sci)
     time.sleep(1)
 
     accept_top_applicant(True)
 
-    high_demand = False
-
     # INTERIOR
-    pg.mouseDown(INTERIOR)
-    pg.mouseUp(INTERIOR)
+    pg.mouseDown(inter)
+    pg.mouseUp(inter)
     time.sleep(1)
 
-    accept_top_applicant(False)
+    accept_top_applicant(True)
 
 pg.FAILSAFE = False
 blacklist = []
 
 use_blacklist = False
+
+# TODO: REMOVE TEST
 
 if use_blacklist:
     with open('blacklist.txt', newline='') as csvfile:
@@ -172,13 +252,6 @@ if use_blacklist:
             blacklist.append(row)
             print(', '.join(row))
 
-# TODO: integrate blacklist
-
-time.sleep(10)
-
-pg.mouseDown(1613, 910)
-pg.mouseUp(1613, 910)
-
 time.sleep(10)
 
 start_time = time.time()
@@ -187,7 +260,12 @@ while True:
     if time.time() - start_time > 3600:
         close_app()
         open_app()
+        go_to_titles()
         start_time = time.time()
     else:
-        accept_loop()
+        # image = pg.screenshot(allScreens=True)
+        # image.show()
+        #TODO: CHANGE THIS EVERY SATURDAY
+        accept_loop(extra_titles=True)
         time.sleep(5)
+
---
+++ FL_bot_2.0.py
@@ -0,0 +1,246 @@
+import os
+import subprocess
+import time
+from datetime import datetime
+from random import random
+
+import PIL.Image
+import cv2
+import pytesseract as pt
+import numpy as np
+from PIL import Image
+
+# POSITIONS (for 1280x720)
+OPEN_GAME = (572, 257)
+
+PROFILE = (53, 63)
+SERVER = (514, 880)
+APPLY = (358, 1095)
+
+FL_TITLE = (145, 722)
+STRAT_TITLE = (355, 722)
+SEC_TITLE = (570, 722)
+DEV_TITLE = (145, 980)
+SCI_TITLE = (355, 980)
+INT_TITLE = (570, 980)
+
+# FL_TITLE_2 = (145, 722)
+# STRAT_TITLE_2 = (355, 722)
+# SEC_TITLE_2 = (570, 722)
+# DEV_TITLE_2 = (145, 780)
+# SCI_TITLE_2 = (355, 780)
+# INT_TITLE_2  = (570, 780)
+
+LIST = (621, 1080)
+CLOSE = (664, 131)
+
+LIST_SCROLL_START = (352, 428)
+LIST_SCROLL_END = (352, 950)
+
+ACCEPT = (540, 294)
+DENY = (614, 294)
+
+# DONT_SHOW_AGAIN = (380, 1050)
+# CONFIRM_DENY = (721, 1215)
+
+CLOSE_ADS = (623, 95)
+
+# Other globals
+
+connectionPort = 5555
+deviceSerial = ""
+
+template = Image.open("instruction_template.png")
+template = np.asarray(template)
+template = cv2.cvtColor(template, cv2.COLOR_RGB2BGR)
+
+# Utils
+def connect():
+    global deviceSerial
+
+    os.system(f"adb connect localhost:{connectionPort}")
+    time.sleep(1)
+
+    devices_string = os.popen("adb devices").read()
+    devices_strings = devices_string.split("\n")
+    print(devices_strings)
+    if len(devices_string) > 1:
+        deviceSerial = devices_strings[1].split("\t")[0]
+        print("Connected to device: " + deviceSerial)
+        if len(deviceSerial) == 0:
+            deviceSerial = None
+    else:
+        deviceSerial = None
+        print("Error: No devices connected")
+
+def tap(x, y):
+    x += random() * 10
+    y += random() * 10
+    os.system(f"adb -s {deviceSerial} shell input tap {x} {y}")
+    time.sleep(2.5 + random())
+
+def quick_tap(x, y):
+    x += random() * 10
+    y += random() * 10
+    os.system(f"adb -s {deviceSerial} shell input tap {x} {y}")
+    time.sleep(0.5 + random())
+
+def long_tap(x, y, d):
+    x += random() * 10
+    y += random() * 10
+    os.system(f"adb -s {deviceSerial} shell input swipe {x} {y} {x} {y} {d}")
+    time.sleep(1 + random())
+
+def swipe(x, y, xx, yy, d):
+    x += random() * 10
+    y += random() * 10
+    os.system(f"adb -s {deviceSerial} shell input swipe {x} {y} {xx} {yy} {d}")
+    time.sleep(0.5 + random())
+
+# Logic
+
+def close_game():
+    # Go to home
+    os.system(f"adb -s {deviceSerial} shell input keyevent KEYCODE_APP_SWITCH")
+    time.sleep(2)
+    swipe(140, 670, 712, 670, 300)
+    time.sleep(2)
+    os.system(f"adb -s {deviceSerial} shell input keyevent KEYCODE_HOME")
+    time.sleep(2)
+
+def restart_app():
+    close_game()
+    open_game()
+    close_ads()
+    open_title_screen()
+
+def open_game():
+    tap(OPEN_GAME[0], OPEN_GAME[1])
+    time.sleep(30)
+
+def close_ads():
+    tap(CLOSE_ADS[0], CLOSE_ADS[1])
+    tap(CLOSE_ADS[0], CLOSE_ADS[1])
+    tap(CLOSE_ADS[0], CLOSE_ADS[1])
+
+def open_title_screen():
+    tap(PROFILE[0], PROFILE[1])
+    tap(SERVER[0], SERVER[1])
+
+def apply_to_fl():
+    tap(FL_TITLE[0], FL_TITLE[1])
+    tap(APPLY[0], APPLY[1])
+
+def open_applicant_list():
+    tap(LIST[0], LIST[1])
+
+def close_list():
+    tap(CLOSE[0], CLOSE[1])
+
+def scroll_up_list():
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    swipe(LIST_SCROLL_START[0], LIST_SCROLL_START[1], LIST_SCROLL_END[0], LIST_SCROLL_END[1], 500)
+    time.sleep(1)
+
+def accept_top_applicant():
+    scroll_up_list()
+    quick_tap(ACCEPT[0], ACCEPT[1])
+    quick_tap(ACCEPT[0], ACCEPT[1])
+    quick_tap(ACCEPT[0], ACCEPT[1])
+    quick_tap(ACCEPT[0], ACCEPT[1])
+    quick_tap(ACCEPT[0], ACCEPT[1])
+
+def on_instruction_screen():
+    global template
+    os.system(f"adb -s {deviceSerial} exec-out screencap -p > test.png")
+    time.sleep(0.2)
+    image = Image.open("test.png")
+    image = image.crop((238, 211, 480, 276))
+    image = np.asarray(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # image = image[238: 480, 211: 276]
+    difference = cv2.subtract(template, image)
+    b, g, r = cv2.split(difference)
+    if cv2.countNonZero(b) < 30 and cv2.countNonZero(g) < 30 and cv2.countNonZero(r) < 30:
+        print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S" + ": " +"On instruction screen"))
+        return True
+    return False
+
+def loop(title_y_offset):
+    if on_instruction_screen():
+        close_list()
+
+    # Secretary of Strategy
+    tap(STRAT_TITLE[0], STRAT_TITLE[1] + title_y_offset)
+    open_applicant_list()
+    accept_top_applicant()
+    if on_instruction_screen():
+        close_list()
+    else:
+        close_list()
+        close_list()
+
+    if on_instruction_screen():
+        close_list()
+
+    # Secretary of Security
+    tap(SEC_TITLE[0], SEC_TITLE[1] + title_y_offset)
+    open_applicant_list()
+    accept_top_applicant()
+    if on_instruction_screen():
+        close_list()
+    else:
+        close_list()
+        close_list()
+
+    if on_instruction_screen():
+        close_list()
+
+    # Secretary of Development
+    tap(DEV_TITLE[0], DEV_TITLE[1] + title_y_offset)
+    open_applicant_list()
+    accept_top_applicant()
+    if on_instruction_screen():
+        close_list()
+    else:
+        close_list()
+        close_list()
+
+    if on_instruction_screen():
+        close_list()
+
+    # Secretary of Science
+    tap(SCI_TITLE[0], SCI_TITLE[1] + title_y_offset)
+    open_applicant_list()
+    accept_top_applicant()
+    if on_instruction_screen():
+        close_list()
+    else:
+        close_list()
+        close_list()
+
+    if on_instruction_screen():
+        close_list()
+
+    # Secretary of Interior
+    tap(INT_TITLE[0], INT_TITLE[1] + title_y_offset)
+    open_applicant_list()
+    accept_top_applicant()
+    close_list()
+    close_list()
+
+connect()
+
+# IMPORTANT: Set before starting
+extra_titles = False
+
+while True:
+    loop(0 if extra_titles else -200)
